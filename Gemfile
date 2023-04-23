251119ff8c65d822865fd04de794fbf6d16882cf
Conner Van Loan <cdvanloan@gmail.com>
2023-04-23 14:58:23 -0600

    sets up mapquest service, method for getting location to latlng

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ group :development, :test do
   gem "webmock"
   gem "factory_bot_rails"
   gem "faker"
+  gem "simplecov"
 end
 
 group :development do
---
+++ spec/api/v1/forecast_request_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+describe 'Forecast' do
+  describe 'happy path' do
+    # it 'can take a location and return a forecast' do
+
+    # end
+  end
+end
