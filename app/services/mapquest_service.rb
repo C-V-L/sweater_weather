9fe6129fc627f31f0e302dbad57a67f5165a2700
Conner Van Loan <cdvanloan@gmail.com>
2023-04-23 15:13:01 -0600

    edits get_coord method to just return the latlng hash

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -1,16 +1,20 @@
 class MapquestService
-  def self.conn
-    Faraday.new('http://www.mapquestapi.com') do |faraday|
-      faraday.headers["key"] = ENV["MAPQUEST_API_KEY"]
-    end
-  end
-
+  
   def self.get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
   
   def self.get_coordinates(location)
-    get_url("/geocoding/v1/address?location=#{location}")
+    response = get_url("/geocoding/v1/address?location=#{location}")
+    response[:results][0][:locations][0][:latLng]
+  end
+
+  private 
+
+  def self.conn
+    Faraday.new('http://www.mapquestapi.com') do |faraday|
+      faraday.headers["key"] = ENV["MAPQUEST_API_KEY"]
+    end
   end
 end
