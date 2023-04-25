b101a7239c2dc60f71d92cbb3616c05b27bb119c
Conner Van Loan <cdvanloan@gmail.com>
2023-04-25 08:36:18 -0600

    adds working post action for user with appropriate json response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,14 +2,14 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: UsersSerializer.new(user), status: :created
+      render json: UsersSerializer.new(user)
     else
-      render json: { error: 'Email already exists' }, status: :conflict
+      render json: { error: 'Email already exists' }, status: 400
     end
   end
 
   private
   def user_params
-    params.permit(:email, :password_digest, :password_confirmation)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password, confirmation: true
   validates_confirmation_of :password
+  validates :api_key, presence: true, uniqueness: true
+
+  before_validation :create_api_key
+
+  def create_api_key
+    self.api_key = SecureRandom.hex(13)
+  end
 end
