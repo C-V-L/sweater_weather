1221d50d92695aef7edf19124159213bd76a5714
Conner Van Loan <cdvanloan@gmail.com>
2023-04-25 18:05:43 -0600

    adds working road_trip method to facade

--- app/facades/map_quest_facade.rb
+++ app/facades/map_quest_facade.rb
@@ -6,7 +6,20 @@ class MapQuestFacade
 
   def road_trip
     route = MapquestService.get_directions(@origin, @destination)
-    weather = WeatherService.get_forecast_weather(route[:route][:locations][1][:latLng][:lat], route[:route][:locations][1][:latLng][:lng])
-    require 'pry'; binding.pry
+    # now i need to calculate the datetime and hour of arrival to get the weather
+    arrival_time_unix = Time.now.to_i + route[:route][:realTime]
+    arrival_date = Time.at(arrival_time_unix).strftime("%Y-%m-%d")
+    arrival_hour = Time.at(arrival_time_unix).strftime("%H")
+    weather = WeatherService.get_timebased_weather(@destination, arrival_date, arrival_hour)
+    { 
+      start_city: @origin, 
+      end_city: @destination, 
+      travel_time: route[:route][:formattedTime], 
+      weather_at_eta: { 
+        datetime: Time.at(arrival_time_unix).strftime("%Y-%m-%d"), 
+        temperature: weather[:forecast][:forecastday][0][:hour][0][:temp_f],
+        conditions: weather[:forecast][:forecastday][0][:hour][0][:condition][:text] 
+        } 
+      }
   end
 end
